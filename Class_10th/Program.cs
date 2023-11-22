30a2c21408d2f68ac832be731abc571f2178294c
7erz <leesncat@naver.com>
2023-11-22 19:47:55 +0900

    231122 무명 형식

---
+++ Class_10th/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Class_10th
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 숙제(등차수열) 
+            //등차수열
+            /*int firstTerm;          //첫항
+            int commonDifferent;    //공차
+            int size;
+
+            //Console.ReadLine() : string 타입으로만 값 반환
+            //string string1 = Console.ReadLine();
+            //string string2 = Console.ReadLine();
+            //firstTerm = Console.ReadLine();
+
+
+            Console.Write("firstTerm :");
+            firstTerm = Int32.Parse(Console.ReadLine());
+            Console.Write("commonDifferent :");
+            commonDifferent = Int32.Parse(Console.ReadLine());
+            Console.Write("size :");
+            size = Int32.Parse(Console.ReadLine());
+
+            //Console.WriteLine("name : " + string1);
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(firstTerm + commonDifferent * i);
+            }*/
+
+            #endregion
+
+            #region 등비 수열
+            /*int i, j;
+            int size;
+            //int sub = 0;
+
+            Console.Write("시작 수: ");
+            i = Int32.Parse(Console.ReadLine());
+            Console.Write("등비: ");
+            j = Int32.Parse(Console.ReadLine());
+            Console.Write("사이즈: ");
+            size = Int32.Parse(Console.ReadLine());
+
+            for (int a = 1; a <= size; a++)
+            {
+                //2시작 3등비 5사이즈 = 2 6 18 
+                if (a == 1)
+                {
+                    sub += i;
+                }
+                else
+                {
+                    sub *= j;
+                }
+
+                Console.WriteLine(sub);
+            }
+
+            for (int a=0; a<size; a++)
+            {
+                Console.WriteLine(i);
+                i *= j;
+            }*/
+
+            #endregion
+
+            #region 무명 형식
+            //이름이 존재하지 않는 객체 또는 변수를 의미함
+            /*var data = new { Name = "Data", Value = 10 };
+
+            //무명 형식의 경우 값을 읽기만 할 수 있음
+            //data.Name = 200; => X
+
+            Console.WriteLine(data.Name);
+            Console.WriteLine(data.Value);*/
+
+            #endregion
+        }
+    }
+}
--- ProgramCS/Program.cs
+++ ProgramCS/Program.cs
@@ -3,36 +3,48 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace ProgramCS
 {
-    public class Homework : IEnumerator
-    {
-        private int index = 0;
-        private int[] number;
-        private int magicNumber;
-
-        public Homework() {
-            index = -1;
-            number = new int[10];
-        }
-
-        public bool MoveNext()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Reset()
-        {
-            throw new NotImplementedException();
-        }
-    }
-
     internal class Program
     {
         static void Main(string[] args)
         {
-            #region 숙제 
-            //등차수열
+            #region 이진 트리
+            //오름차순으로 정렬된 리스트에서 특정한 값의 위치를 찾는 알고리즘
+            //시간 복잡도 : log(n)
+
+            /*
+            //찾아야 하는 수 2
+            //[1][2][3][4][5][6]
+            // 0    ↑         5
+            //left = 0 right = 5
+            //0+5 후 나누기 = index값 
+
+            //[1][2][3]
+            // 0  ↑ 2
+
+            //3을 찾을때는 0은 없애고
+            //[1][2][3]
+            //    1  2
+
+            //비교는 인덱스로 찾는건 수로
+            //크면 left 작으면 right를 갱신
+            */
+
+            
+
+
+            //1. 배열의 가운데 요소에 인덱스 pivot으로 설정
+
+            //2. [pivot]의 값이 찾고자 하는 요소가 같다면 검색 완료
+
+            //3. [pivot]의 값이 찾는 값보다 크다면 left~pivot 사이 검색
+
+            //4. [pivot]의 값이 찾는 값보다 작다면 pivot~right 사이 검색
+
+
+
 
             #endregion
+
         }
 
     }
