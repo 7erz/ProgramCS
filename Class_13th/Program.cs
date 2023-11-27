0834f4d9e31434e7199e424b013b9bfadacf8ddd
7erz <leesncat@naver.com>
2023-11-27 19:47:59 +0900

    231127 그래프

---
+++ Class_13th/Program.cs
@@ -0,0 +1,213 @@
+﻿namespace Class_13th
+{
+    internal class Program
+    {
+        static void MergeSort(int[] array, int start, int end)
+        {
+            if (start < end)
+            {
+                int middle = (start + end) / 2;
+
+                MergeSort(array, start, middle);
+                MergeSort(array, middle + 1, end);
+
+                Merge(array, start, middle, end);
+            }
+        }
+        static void Merge(int[] array, int start, int middle, int end)
+        {
+            int[] leftArray = new int[middle - start + 1];
+            int[] rightArray = new int[end - middle];
+
+            int index = start;
+            int left = 0;
+            int right = 0;
+
+            for (left = 0; index <= middle; left++)
+            {
+                leftArray[left] = array[index++];
+            }
+
+            for (right = 0; index <= end; right++)
+            {
+                rightArray[right] = array[index++];
+            }
+
+            index = start;
+
+            left = 0;
+            right = 0;
+
+            while (left < leftArray.Length && right < rightArray.Length)
+            {
+                if (leftArray[left] < rightArray[right])
+                {
+                    array[index] = leftArray[left++];
+                }
+                else
+                {
+                    array[index] = rightArray[right++];
+                }
+
+                index++;
+            }
+
+            while (left < leftArray.Length)
+            {
+                array[index++] = leftArray[left++];
+            }
+
+            while (right < rightArray.Length)
+            {
+                array[index++] = rightArray[right++];
+            }
+        }
+        
+        
+        
+        //나의 답
+        static void Split(int[] array)
+        {
+            if (array.Length <= 1)
+                return;
+
+            int mid = array.Length / 2;
+            int[] leftArray = new int[mid];
+            int[] rightArray = new int[array.Length - mid];
+
+            Array.Copy(array, 0, leftArray, 0, mid);
+            Array.Copy(array, mid, rightArray, 0, array.Length - mid);
+
+            Split(leftArray);
+            Split(rightArray);
+            Sort(array, leftArray, rightArray);
+        }
+
+        static void Sort1(int[] array, int[] array1, int[] array2)
+        {
+
+            int[] sortArray = new int[array1.Length + array2.Length];
+            int left = 0;
+            int right = 0;
+            int counter = 0;
+
+            if (array1.Length == 1 && array2.Length == 1)
+            {
+                if (array1[0] > array2[0])
+                {
+                    sortArray[0] = array2[0];
+                    sortArray[1] = array1[0];
+                }
+                else
+                {
+                    sortArray[0] = array1[0];
+                    sortArray[1] = array2[0];
+                }
+            }
+            else
+            {
+                while (counter == sortArray.Length - 1)
+                {
+                    if (array1[left] > array2[right])
+                    {
+                        sortArray[counter] = array1[left];
+                        counter++;
+                        left++;
+                    }
+                    else
+                    {
+                        sortArray[counter] = array2[right];
+                        counter++;
+                        right++;
+                    }
+                }
+
+            }
+        }
+
+        static void Sort(int[] array, int[] leftArray, int[] rightArray)
+        {
+            int leftIndex = 0, rightIndex = 0, mergedIndex = 0;
+
+            while (leftIndex < leftArray.Length && rightIndex < rightArray.Length)
+            {
+                if (leftArray[leftIndex] <= rightArray[rightIndex])
+                {
+                    array[mergedIndex] = leftArray[leftIndex];
+                    leftIndex++;
+                }
+                else
+                {
+                    array[mergedIndex] = rightArray[rightIndex];
+                    rightIndex++;
+                }
+                mergedIndex++;
+            }
+
+            while (leftIndex < leftArray.Length)
+            {
+                array[mergedIndex] = leftArray[leftIndex];
+                leftIndex++;
+                mergedIndex++;
+            }
+
+            while (rightIndex < rightArray.Length)
+            {
+                array[mergedIndex] = rightArray[rightIndex];
+                rightIndex++;
+                mergedIndex++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            #region 병합 정렬
+            //하나의 리스트를 두 개의 균등한 크기로 분할하고
+            //분할된 부분 리스트를 정렬한 다음, 두 개의 정렬된 부분
+            //리스트를 합하여 전체가 정렬된 리스트가 되게 하는 방법
+
+            //분할 : 입력 배열을 같은 크기의 2개의 부분 배열로 분할
+
+            //정복 : 부분 배열을 정렬하며, 부분 배열의 크기가 충분히 작지 않으면
+            //순환 호출을 이용하여 다시 분할 정복 실행
+
+            //결합 : 정렬된 부분 배열들을 하나의 배열에 병합
+
+            // 하는 법
+            /*
+            //[21][10][12][20][25][13][15][22]
+            //이 상태에서 반으로 쪼갬
+            //[21][10][12][20]   /   [25][13][15][22]
+            //[21][10]  /   [12][20]   /    [25][13]    /   [15][22]
+            //[21]    [10]    [12]    [20]    [25]    [13]    [15]    [22]
+
+            //값이 작다면 왼쪽에 넣어주기
+            //[10][21]  /   [12][20]    /   [13][25]    /   [15][22]
+
+            //4개짜리 배열은 LEFT([*10][21]) RIGHT([*12][20]) 을 비교
+            //*표시중 제일 작은 것을 0번째 배열에 넣음
+            //그후 LEFT를 1로 옮김[21]
+            //[*12]가 작기에 1에 [12]를 넣음
+            //그후 [21],[20]을 비교해서 2의 자리에 [20]을 넣음
+            //나머지를 3의 배열에 넣기
+            //8개 짜리도 똑같이 하면됨
+            */
+
+
+            int[] array = new int[] { 21, 10, 12, 20, 25, 13, 15, 22 };
+            MergeSort(array, 0, array.Length - 1);
+            //Split(array);
+
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+
+
+
+
+
+
+            #endregion
+        }
+    }
+}
--- ProgramCS/Program.cs
+++ ProgramCS/Program.cs
@@ -2,152 +2,41 @@
 {
     internal class Program
     {
-        static void Split(int[] array)
-        {
-            if (array.Length <= 1)
-                return;
-
-            int mid = array.Length / 2;
-            int[] leftArray = new int[mid];
-            int[] rightArray = new int[array.Length - mid];
-
-            Array.Copy(array, 0, leftArray, 0, mid);
-            Array.Copy(array, mid, rightArray, 0, array.Length - mid);
-
-            Split(leftArray);
-            Split(rightArray);
-            Sort(array, leftArray, rightArray);
-        }
-
-        //나의 답
-        static void Sort1(int[] array, int[] array1, int[] array2)
-        {
-
-            int[] sortArray = new int[array1.Length + array2.Length];
-            int left = 0;
-            int right = 0;
-            int counter = 0;
-
-            if (array1.Length == 1 && array2.Length == 1)
-            {
-                if (array1[0] > array2[0])
-                {
-                    sortArray[0] = array2[0];
-                    sortArray[1] = array1[0];
-                }
-                else
-                {
-                    sortArray[0] = array1[0];
-                    sortArray[1] = array2[0];
-                }
-            }
-            else
-            {
-                while (counter == sortArray.Length - 1)
-                {
-                    if (array1[left] > array2[right])
-                    {
-                        sortArray[counter] = array1[left];
-                        counter++;
-                        left++;
-                    }
-                    else
-                    {
-                        sortArray[counter] = array2[right];
-                        counter++;
-                        right++;
-                    }
-                }
-
-            }
-        }
-
-        static void Sort(int[] array, int[] leftArray, int[] rightArray)
-        {
-            int leftIndex = 0, rightIndex = 0, mergedIndex = 0;
-
-            while (leftIndex < leftArray.Length && rightIndex < rightArray.Length)
-            {
-                if (leftArray[leftIndex] <= rightArray[rightIndex])
-                {
-                    array[mergedIndex] = leftArray[leftIndex];
-                    leftIndex++;
-                }
-                else
-                {
-                    array[mergedIndex] = rightArray[rightIndex];
-                    rightIndex++;
-                }
-                mergedIndex++;
-            }
-
-            while (leftIndex < leftArray.Length)
-            {
-                array[mergedIndex] = leftArray[leftIndex];
-                leftIndex++;
-                mergedIndex++;
-            }
-
-            while (rightIndex < rightArray.Length)
-            {
-                array[mergedIndex] = rightArray[rightIndex];
-                rightIndex++;
-                mergedIndex++;
-            }
-        }
-
         static void Main(string[] args)
         {
-            #region 병합 정렬
-            //하나의 리스트를 두 개의 균등한 크기로 분할하고
-            //분할된 부분 리스트를 정렬한 다음, 두 개의 정렬된 부분
-            //리스트를 합하여 전체가 정렬된 리스트가 되게 하는 방법
+            #region 그래프
+            //정점(Vertex)과 간선(Edge)들이 서로 복잡하게 연결되어 있는 자료구조
 
-            //분할 : 입력 배열을 같은 크기의 2개의 부분 배열로 분할
+            //정점(Vertex)
+            //노드라고도 하며, 데이터가 저장되는 그래프의 기본 원소
 
-            //정복 : 부분 배열을 정렬하며, 부분 배열의 크기가 충분히 작지 않으면
-            //순환 호출을 이용하여 다시 분할 정복 실행
+            //간선(Edge)
+            //링크(link)라고도 하며, 정점 간의 관계를 나타냄
 
-            //결합 : 정렬된 부분 배열들을 하나의 배열에 병합
+            //인접 정점(adjacent vertex)
+            //하나의 정점에서 간선에 의해 직접 연결되어 있는 정점
+            //[C] <=> [D]
 
-            // 하는 법
-            /*
-            //[21][10][12][20][25][13][15][22]
-            //이 상태에서 반으로 쪼갬
-            //[21][10][12][20]   /   [25][13][15][22]
-            //[21][10]  /   [12][20]   /    [25][13]    /   [15][22]
-            //[21]    [10]    [12]    [20]    [25]    [13]    [15]    [22]
+            //차수 (Degree)
+            //정점에 연결되어 있는 간선의 수를 의미
 
-            //값이 작다면 왼쪽에 넣어주기
-            //[10][21]  /   [12][20]    /   [13][25]    /   [15][22]
+            //진입 차수
+            //외부에서 오는 간선의 수
 
-            //4개짜리 배열은 LEFT([*10][21]) RIGHT([*12][20]) 을 비교
-            //*표시중 제일 작은 것을 0번째 배열에 넣음
-            //그후 LEFT를 1로 옮김[21]
-            //[*12]가 작기에 1에 [12]를 넣음
-            //그후 [21],[20]을 비교해서 2의 자리에 [20]을 넣음
-            //나머지를 3의 배열에 넣기
-            //8개 짜리도 똑같이 하면됨
-            */
+            //진출 차수
+            //외부로 향하는 간선의 수
 
+            //단순 경로
+            //경로 중에서 반복되는 간선이 없는 경로
 
-            int[] array = new int[8] { 21,10,12,20,25,13,15,22};    
-            Split(array);
+            //사이클(cycle)
+            //시작 정점과 종료 정점이 같은 단순 경로
+            #endregion
 
-            foreach (int i in array)
-            {
-                Console.Write(i + " ");
-            }
-
-
-
-
-
+            #region 인접 행렬
 
             #endregion
         }
-
     }
-
 }
 
